938b9bf476bfd42e94e2b861b64180b1bdd45a30
Barny <BarnyK@users.noreply.github.com>
2019-05-15 20:11:36 +0200

    Initial Commit

---
+++ BinaryTree1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinaryTree1
+//
+//  Created by Winky Face on 15/05/2019.
+//  Copyright © 2019 Barnaba Krupowicz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
